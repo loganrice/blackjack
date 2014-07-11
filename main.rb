74b9c0eac07715534a58dd6008ce87ec2e986179
Logan Rice <loganrice72@gmail.com>
2014-07-11 12:28:28 -0700

    buttons and actions

--- main.rb
+++ main.rb
@@ -3,15 +3,52 @@ require 'sinatra'
 
 set :sessions, true
 
+helpers do
+  def calculate_total(cards)
+    total = 0
+    ace_counter = 0
+    cards.each do |card|
+      suit = card[0]
+      rank = card[1]
+      if rank.is_a? String
+        if rank == "ace"
+          ace_counter += 1
+          total += 11
+        else
+          total += 10
+        end
+      else
+        total += rank
+      end 
+    end
+
+    # adjust for aces
+    ace_counter.times do |a|
+      if total <= 21
+        break
+      else
+        total -= 10
+      end
+    end
+    total
+  end
+
+  def card_path(card)
+    suit = card[0]
+    rank = card[1]
+    "/images/cards/#{suit}_#{rank}.jpg"
+  end
+end
+
+before do
+  @show_buttons = true
+end
+
 get "/" do
   if session[:name]
-
-    SUITS = ["Hearts", "Diamonds", "Clubs", "Spades"]
-    RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, "ace", "jack", "queen", "king"]
-    session[:deck] = SUITS.product(RANKS)
-    erb :game
+    redirect '/game'
   else
-    redirect "login"
+    redirect "/login"
   end
 end
 
@@ -21,5 +58,31 @@ end
 
 post "/" do 
   session[:name] = params[:name]
-  redirect "/"
+  redirect "/game"
+end
+
+get "/game" do 
+  suits = ["hearts", "diamonds", "clubs", "spades"]
+  ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "ace", "jack", "queen", "king"]
+  session[:deck] = suits.product(ranks).shuffle!
+  session[:player_cards] = []
+  session[:dealer_cards] = []
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  erb :game
+end
+
+post "/game/player/hit" do
+  session[:player_cards] << session[:deck].pop
+  if calculate_total(session[:player_cards]) > 21
+    @error = "Sorry it looks like you have busted."
+    @show_buttons = false
+  end
+  erb :game
+end
+
+post "/game/player/stay" do
+  @show_buttons = false
 end
